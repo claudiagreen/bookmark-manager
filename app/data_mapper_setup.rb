deed37f5bed22a860c4a17bbec9238a5e1746519
Claudia Green <claudia@claudiag.me.uk>
2016-03-25 14:41:07 +0000

    Challenge 19 complete.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -8,3 +8,4 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
+DataMapper.auto_migrate!
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,6 +1,9 @@
-def sign_up
+def sign_up(email: 'test@test.com',
+            password: 'test',
+            password_confirmation: 'test')
   visit '/users/new'
-  fill_in :email, with: "anthony@gmail.com"
-  fill_in :password, with: "password"
-  click_button "submit"
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
+  click_button 'submit'
 end
