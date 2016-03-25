745713226263f939ffe443d6995357a282aa545b
Claudia Green <claudia@claudiag.me.uk>
2016-03-25 14:04:42 +0000

    Small changes to web helper

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -20,3 +20,10 @@ def sign_up_bad_password
   fill_in :password, with: "password"
   fill_in :password_confirmation, with: "pissword"
 end
+
+def sign_in(email:, password:)
+  visit '/links'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'submit'
+end
